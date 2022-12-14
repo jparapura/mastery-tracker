89fed2de7373d45bba8294bd8c66d72f94f36d57
Jakub Parapura <jakubparapura@gmail.com>
2022-12-14 21:57:24 +0100

    :construction: register page

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, RegisterPage
 
 urlpatterns = [
-    path('home/', HomePage, name='home-page')
+    path('home/', HomePage, name='home-page'),
+    path('register/', RegisterPage, name='register-page')
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def HomePage(request):
     return render(request, 'index.html', {})
 
-def Register(request):
-    pass
+def RegisterPage(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('pass')
 
-def Login(request):
+        new_user = User.objects.create_user(name, email, password)
+        # new_user.first_name = fname
+
+        new_user.save()
+        return redirect('home-page')
+
+
+    return render(request, 'register.html', {})
+
+def LoginPage(request):
     pass
